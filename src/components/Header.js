699d65931b694ada771ace16dc18fd8fc5c4e485
Jay <jjjjuhyuneee@naver.com>
2024-05-18 22:00:24 +0900

    0518

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,14 +4,14 @@ function Header() {
   return (
     <div className="header_all_wrap">
       <div className="header_wrap">
-        <a href="/" className="header_main_logo">
+        <a href="/morement" className="header_main_logo">
           Morement
         </a>
 
         <div className="header_nav">
-          <a href="/">portfolio</a>
-          <a href="/">contact</a>
-          <a href="/">resume</a>
+          <a href="/morement">portfolio</a>
+          <a href="/morement">contact</a>
+          <a href="/morement">resume</a>
         </div>
       </div>
     </div>
