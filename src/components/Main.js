46824ada70247bcd2cc0e49cb08e651c1c8c3557
Jay <jjjjuhyuneee@naver.com>
2024-05-23 22:15:15 +0900

    0523

--- src/components/Main.js
+++ src/components/Main.js
@@ -9,13 +9,18 @@ function Main() {
             autoPlay
             loop
             muted
-            src={process.env.PUBLIC_URL + "/images/main/main2.mp4"}
+            src={process.env.PUBLIC_URL + "/images/main/main3.mp4"}
             className="main_visual_video"
           />
-          <div className="main_title_wrap">
+          {/* <div className="main_title_wrap1">
             <div className="main_title main_title1">BREAK</div>
             <div className="main_title main_title2">OUT OF THE</div>
             <div className="main_title main_title3">FRAME</div>
+          </div> */}
+          <div className="main_title_wrap2">
+            <div className="main_title main_title1">I always try to craft </div>
+            <div className="main_title main_title2">new experiences through challenges </div>
+            <div className="main_title main_title3">and immersion.</div>
           </div>
         </div>
       </div>
