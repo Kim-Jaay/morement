299bf28453d372d69d77d91b0e8e9d136d5887cd
Jay <jjjjuhyuneee@naver.com>
2024-06-08 00:04:02 +0900

    0608

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,12 +1,153 @@
 import React from "react";
 import "../css/projects.scss";
 
-
 function Projects() {
   return (
     <div className="wfull">
       <div className="project_all_wrap">
         <div>Projects</div>
+        <div className="gap"></div>
+        <div className="project_wrap">
+          <div>
+            <img
+              src={
+                process.env.PUBLIC_URL +
+                "/images/project/thumbnail/thumb_14.png"
+              }
+              alt=""
+            />
+          </div>
+
+          <div>
+            <img
+              src={
+                process.env.PUBLIC_URL +
+                "/images/project/thumbnail/thumb_13.png"
+              }
+              alt=""
+            />
+          </div>
+
+          <div>
+            <img
+              src={
+                process.env.PUBLIC_URL +
+                "/images/project/thumbnail/thumb_12.png"
+              }
+              alt=""
+            />
+          </div>
+
+          <div>
+            <img
+              src={
+                process.env.PUBLIC_URL +
+                "/images/project/thumbnail/thumb_11.png"
+              }
+              alt=""
+            />
+          </div>
+
+          <div>
+            <img
+              src={
+                process.env.PUBLIC_URL +
+                "/images/project/thumbnail/thumb_10.png"
+              }
+              alt=""
+            />
+          </div>
+
+          <div>
+            <img
+              src={
+                process.env.PUBLIC_URL +
+                "/images/project/thumbnail/thumb_09.png"
+              }
+              alt=""
+            />
+          </div>
+
+          <div>
+            <img
+              src={
+                process.env.PUBLIC_URL +
+                "/images/project/thumbnail/thumb_08.png"
+              }
+              alt=""
+            />
+          </div>
+
+          <div>
+            <img
+              src={
+                process.env.PUBLIC_URL +
+                "/images/project/thumbnail/thumb_07.png"
+              }
+              alt=""
+            />
+          </div>
+
+          <div>
+            <img
+              src={
+                process.env.PUBLIC_URL +
+                "/images/project/thumbnail/thumb_06.png"
+              }
+              alt=""
+            />
+          </div>
+
+          <div>
+            <img
+              src={
+                process.env.PUBLIC_URL +
+                "/images/project/thumbnail/thumb_05.png"
+              }
+              alt=""
+            />
+          </div>
+
+          <div>
+            <img
+              src={
+                process.env.PUBLIC_URL +
+                "/images/project/thumbnail/thumb_04.png"
+              }
+              alt=""
+            />
+          </div>
+
+          <div>
+            <img
+              src={
+                process.env.PUBLIC_URL +
+                "/images/project/thumbnail/thumb_03.png"
+              }
+              alt=""
+            />
+          </div>
+
+          <div>
+            <img
+              src={
+                process.env.PUBLIC_URL +
+                "/images/project/thumbnail/thumb_03.png"
+              }
+              alt=""
+            />
+          </div>
+
+          <div>
+            <img
+              src={
+                process.env.PUBLIC_URL +
+                "/images/project/thumbnail/thumb_01.png"
+              }
+              alt=""
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
