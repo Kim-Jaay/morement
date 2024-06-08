4602ded65e44f5ced0dd43b1994591ddc1ae1b5b
Jay <jjjjuhyuneee@naver.com>
2024-06-08 23:34:21 +0900

    0608

--- src/components/Main.js
+++ src/components/Main.js
@@ -14,18 +14,20 @@ function Main() {
             src={process.env.PUBLIC_URL + "/images/main/main3.mp4"}
             className="main_visual_video"
           />
-          <div
-            className="main_title_wrap1 wfull"
-            data-aos="fade-up"
-             
-          >
+          <div className="main_title_wrap1 wfull" data-aos="fade-up">
+            
             <div className="main_title main_title1-1">
-              I always try to craft{" "}
+              I always try to craft
             </div>
+
             <div className="main_title main_title1-2">
-              new experiences through challenges{" "}
+              new experiences through challenges
             </div>
-            <div className="main_title main_title1-3">and immersion.</div>
+
+            <div className="main_title main_title1-3">
+              and immersion.
+            </div>
+
           </div>
         </div>
       </div>
