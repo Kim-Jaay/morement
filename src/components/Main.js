d154dd0dba7a5b51df43bb81bd8b703f4fc83d6b
Jay <jjjjuhyuneee@naver.com>
2024-06-08 16:21:53 +0900

    0608

--- src/components/Main.js
+++ src/components/Main.js
@@ -12,11 +12,6 @@ function Main() {
             src={process.env.PUBLIC_URL + "/images/main/main3.mp4"}
             className="main_visual_video"
           />
-          {/* <div className="main_title_wrap1">
-            <div className="main_title main_title1-1">BREAK</div>
-            <div className="main_title main_title1-2">OUT OF THE</div>
-            <div className="main_title main_title1-3">FRAME</div>
-          </div> */}
           <div className="main_title_wrap2 wfull">
             <div className="main_title main_title2-1">I always try to craft </div>
             <div className="main_title main_title2-2">new experiences through challenges </div>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,152 +1,40 @@
-import React from "react";
+import React, { useEffect } from 'react'
+import { Link } from "react-router-dom";
 import "../css/projects.scss";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import MainProject from "../data/List.js";
 
 function Projects() {
+    useEffect(() => {
+        AOS.init({});
+    }, [])
   return (
     <div className="wfull">
       <div className="project_all_wrap">
         <div className="project_title">The Main Projects of Morement</div>
         <div className="sgap"></div>
         <div className="project_wrap">
-          <div>
-            <img
-              src={
-                process.env.PUBLIC_URL +
-                "/images/project/thumbnail/thumb_14.png"
-              }
-              alt=""
-            />
-          </div>
-
-          <div>
-            <img
-              src={
-                process.env.PUBLIC_URL +
-                "/images/project/thumbnail/thumb_13.png"
-              }
-              alt=""
-            />
-          </div>
-
-          <div>
-            <img
-              src={
-                process.env.PUBLIC_URL +
-                "/images/project/thumbnail/thumb_12.png"
-              }
-              alt=""
-            />
-          </div>
-
-          <div>
-            <img
-              src={
-                process.env.PUBLIC_URL +
-                "/images/project/thumbnail/thumb_11.png"
-              }
-              alt=""
-            />
-          </div>
-
-          <div>
-            <img
-              src={
-                process.env.PUBLIC_URL +
-                "/images/project/thumbnail/thumb_10.png"
-              }
-              alt=""
-            />
-          </div>
-
-          <div>
-            <img
-              src={
-                process.env.PUBLIC_URL +
-                "/images/project/thumbnail/thumb_09.png"
-              }
-              alt=""
-            />
-          </div>
-
-          <div>
-            <img
-              src={
-                process.env.PUBLIC_URL +
-                "/images/project/thumbnail/thumb_08.png"
-              }
-              alt=""
-            />
-          </div>
-
-          <div>
-            <img
-              src={
-                process.env.PUBLIC_URL +
-                "/images/project/thumbnail/thumb_07.png"
-              }
-              alt=""
-            />
-          </div>
-
-          <div>
-            <img
-              src={
-                process.env.PUBLIC_URL +
-                "/images/project/thumbnail/thumb_06.png"
-              }
-              alt=""
-            />
-          </div>
-
-          <div>
-            <img
-              src={
-                process.env.PUBLIC_URL +
-                "/images/project/thumbnail/thumb_05.png"
-              }
-              alt=""
-            />
-          </div>
-
-          <div>
-            <img
-              src={
-                process.env.PUBLIC_URL +
-                "/images/project/thumbnail/thumb_04.png"
-              }
-              alt=""
-            />
-          </div>
-
-          <div>
-            <img
-              src={
-                process.env.PUBLIC_URL +
-                "/images/project/thumbnail/thumb_03.png"
-              }
-              alt=""
-            />
-          </div>
-
-          <div>
-            <img
-              src={
-                process.env.PUBLIC_URL +
-                "/images/project/thumbnail/thumb_02.png"
-              }
-              alt=""
-            />
-          </div>
-
-          <div>
-            <img
-              src={
-                process.env.PUBLIC_URL +
-                "/images/project/thumbnail/thumb_01.png"
-              }
-              alt=""
-            />
-          </div>
+          {MainProject.map((it, idx) => {
+            return (
+              <li
+                key={idx}
+                data-aos="fade-up"
+                data-aos-delay={50 * idx}
+                data-aos-duration="300"
+              >
+                <Link to={it.link}>
+                  <img
+                    src={
+                      process.env.PUBLIC_URL +
+                      `/images/project/thumbnail/thumb_0${idx + 1}.png`
+                    }
+                    alt=""
+                  />
+                </Link>
+              </li>
+            );
+          }).reverse()}
         </div>
       </div>
     </div>
